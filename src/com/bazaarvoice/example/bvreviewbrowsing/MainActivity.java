9544292e73bc6134d5b943a1c806aa3be5d4b3d7
ralph.pina <ralph.pina@gmail.com>
2013-05-24 14:28:02 -0500

    Top level categories and data being pulled down.
    - Data being pulled down and and properly sorted and displayed.
    - added the BazaarUIThreadResponsed used in other projects to clean the
    code a bit.

--- src/com/bazaarvoice/example/bvreviewbrowsing/MainActivity.java
+++ src/com/bazaarvoice/example/bvreviewbrowsing/MainActivity.java
@@ -1,18 +1,19 @@
 package com.bazaarvoice.example.bvreviewbrowsing;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.Menu;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bazaarvoice.BazaarRequest;
@@ -21,12 +22,11 @@ import com.bazaarvoice.OnBazaarResponse;
 import com.bazaarvoice.types.Equality;
 import com.bazaarvoice.types.RequestType;
 
-public class MainActivity extends Activity implements OnBazaarResponse {
+public class MainActivity extends Activity {
 
-	private static final String TAG = "MainActivity";
+	//private static final String TAG = "MainActivity";
 	
 	private TextView textView;
-	private Map<JSONObject, LinkedList<String>> categoryTree;
 	private Map<String, JSONObject> allCategories;
 	
 	
@@ -39,33 +39,27 @@ public class MainActivity extends Activity implements OnBazaarResponse {
 	private BazaarRequest request;
 	private DisplayParams params;
 	
+	RelativeLayout.LayoutParams layoutParams;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+	
 		textView = (TextView) findViewById(R.id.textView);
+		textView.setText("Yo! We are loading your data!");
 		textView.setMovementMethod(new ScrollingMovementMethod());
-		categoryTree = new LinkedHashMap<JSONObject, LinkedList<String>>();
+
+		layoutParams = (RelativeLayout.LayoutParams)textView.getLayoutParams();
+		layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, 1);
+		textView.setLayoutParams(layoutParams);
+		
 		allCategories = new LinkedHashMap<String, JSONObject>();
 		initialItemCount = 0;
 		processedItemCount = 0;
 		batchSize = 100;
-		
-		/*
-		 * BazaarRequest will be used to make our API calls. The parameters are set in the BVReviewBrowsingApplication class
-		 */
-		request = new BazaarRequest(BVReviewBrowsingApplication.domain, BVReviewBrowsingApplication.passKey, BVReviewBrowsingApplication.apiVersion);
-		
-		/*
-		 * Get all the top level categories and sort on their name
-		 */
-		//params.addFilter("ParentId", Equality.EQUAL, new String[]{"600610","600630"});
-		
-		/*
-		 * This request will get first 100
-		 */
-		request.sendDisplayRequest(RequestType.CATEGORIES, params, this);		
+
+		execParseTopCategories();
 		
 	}
 
@@ -75,97 +69,190 @@ public class MainActivity extends Activity implements OnBazaarResponse {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	@Override
-	public void onException(String message, Throwable exception) {
-		Log.e(TAG, "Error = "+ message + "\n");
-		exception.printStackTrace();
-	}
-
-	@Override
-	public void onResponse(JSONObject data) {
-		final JSONObject json = data;
-		this.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					//TODO make this a dialog box
-					if (json.getBoolean("HasErrors")) {
-						String hasErrors = "JSON response has errors";
-						JSONArray errors = json.getJSONArray("Errors");
-						StringBuilder errorList = new StringBuilder();
-						
-						for (int i = 0; i < errors.length(); i++) {
-							errorList.append(errors.get(i));
-						}
-						
-						textView.setText(hasErrors + " : " + errorList.toString());
-						
-					} else {
-						/*
-						 * If this is the first time this request is being called, 
-						 * get the total number of results we will need to iterate.
-						 */
-						Log.e(TAG, "initialItemCount in onResponse = " + initialItemCount);
-						if (initialItemCount == 0) {
-							initialItemCount = json.getInt("TotalResults");
-						}
-						
-						itemsToProcess = json.getJSONArray("Results");
-						parseTopCategories();
-						
-					}
-				} catch (JSONException e) {
-					Log.e(TAG, "Error = "+ e.getMessage() + "\n");
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 	
-	private void parseTopCategories() {
+	private void parseTopCategories(OnBazaarResponse response) {
 		/*
-		 * Loop to get all the categories and dump them into our map
+		 * BazaarRequest will be used to make our API calls. The parameters are set in the BVReviewBrowsingApplication class
+		 */
+		request = new BazaarRequest(BVReviewBrowsingApplication.domain, BVReviewBrowsingApplication.passKey, BVReviewBrowsingApplication.apiVersion);
+		
+		/*
+		 * Get all the top level categories and sort on their name
 		 */
 		params = new DisplayParams();
+		params.setOffset(processedItemCount);
 		params.setLimit(batchSize);
 		params.addSort("Name", true);
 		params.addFilter("ParentId", Equality.EQUAL, "null");
 		
-		JSONObject currentObj;
-		for (int i = 0; i < itemsToProcess.length(); i++) {
+		/*
+		 * This request will get first 100
+		 */
+		request.sendDisplayRequest(RequestType.CATEGORIES, params, response);
+	}
+	
+	private void execParseTopCategories() {
+		
+		initialItemCount = 0;
+		processedItemCount = 0;
+		
+		/*
+		 * We will call the method and pass it the listener to execute when the
+		 * request's data returns
+		 */
+		parseTopCategories(new BazaarUIThreadResponse(this) {
+			
+			@Override
+			public void onUiResponse(JSONObject response) {
+				try {
+					itemsToProcess = response.getJSONArray("Results");
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				JSONObject currentObj;
+				JSONArray children;
+				for (int i = 0; i < itemsToProcess.length(); i++) {
+					try {
+						currentObj = itemsToProcess.getJSONObject(i);
+						children = new JSONArray();
+						currentObj.put("Children", children);
+						currentObj.put("HasChildren", false);
+						allCategories.put(currentObj.getString("Id"), currentObj);
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}			
+					processedItemCount++;
+				}
+				
+				if (initialItemCount > processedItemCount) {
+					execParseTopCategories();
+				} else {
+					execParseChildrenCategories();
+				}
+			}
+		});	
+		
+	}
+	
+	private void parseChildrenCategories(OnBazaarResponse response) {
+		 
+		/*
+		 * BazaarRequest will be used to make our API calls. The parameters are set in the BVReviewBrowsingApplication class
+		 */
+		request = new BazaarRequest(BVReviewBrowsingApplication.domain, BVReviewBrowsingApplication.passKey, BVReviewBrowsingApplication.apiVersion);
+		
+		/*
+		 * Get all the top child categories
+		 */
+		params = new DisplayParams();
+		params.setOffset(processedItemCount);
+		params.setLimit(batchSize);
+		ArrayList<String> topCategoryIds = new ArrayList<String>();
+		
+		for (JSONObject id : allCategories.values()) {
 			try {
-				currentObj = itemsToProcess.getJSONObject(i);					
-				allCategories.put(currentObj.getString("Id"), currentObj);
+				topCategoryIds.add(id.getString("Id"));
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}			
-			processedItemCount++;
-		}	
-		
-		if (processedItemCount < initialItemCount) {
-			params.setOffset(processedItemCount);
-			request.sendDisplayRequest(RequestType.CATEGORIES, params, this);
-		} else {
-			
-			params = new DisplayParams();
-			params.setLimit(batchSize);
-			params.addSort("Name", true);
-			params.addFilter("ParentId", Equality.EQUAL, (String[]) allCategories.keySet().toArray());
-			
-			parseNextLevel(params);
+			}
 		}
 		
-		Log.e(TAG, "processedItemCount = " + processedItemCount);
+		String[] ids = new String[topCategoryIds.size()];
+		topCategoryIds.toArray(ids);
+		
+		params.addFilter("ParentId", Equality.EQUAL, ids);
+		
+		/*
+		 * This request will get first 100
+		 */
+		request.sendDisplayRequest(RequestType.CATEGORIES, params, response);
+		
 	}
 	
+	private void execParseChildrenCategories() {
+		
+		initialItemCount = 0;
+		processedItemCount = 0;
+		
+		parseChildrenCategories(new BazaarUIThreadResponse(this) {
+			
+			@Override
+			public void onUiResponse(JSONObject response) {
+				try {
+					itemsToProcess = response.getJSONArray("Results");
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				JSONObject currentObj;
+				JSONArray children;
+				JSONObject parent;
+				for (int i = 0; i < itemsToProcess.length(); i++) {
+					try {
+						currentObj = itemsToProcess.getJSONObject(i);
+						children = new JSONArray();
+						currentObj.put("Children", children);
+						currentObj.put("HasChildren", false);
+						allCategories.put(currentObj.getString("Id"), currentObj);
+						
+						parent = allCategories.get(currentObj.getString("ParentId"));
+						parent.put("HasChildren", true);
+						children = parent.getJSONArray("Children");
+						children.put(currentObj.getString("Id"));
+						
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}			
+					processedItemCount++;
+				}
+				
+				if (initialItemCount > processedItemCount) {
+					execParseChildrenCategories();
+				} else {
+					displayCategories();
+				}
+			}
+		});	
+		
+	}
 	
-	private void parseNextLevel(DisplayParams params) {
-		JSONObject currentObj;
-		for (String Id : allCategories.keySet()) {
-
+	//TODO remove this
+	@SuppressLint("NewApi")
+	private void displayCategories() {
+		StringBuilder tree = new StringBuilder();
+		boolean hasChildren;
+		JSONArray children;
+		
+		for (JSONObject obj : allCategories.values()) {
+			try {
+				hasChildren = obj.getBoolean("HasChildren");
+				
+				if (hasChildren) {
+					tree.append(obj.getString("Name") + "\n");
+					children = obj.getJSONArray("Children");
+					
+					for (int i = 0; i < children.length(); i++) {
+						String childID = (String) children.get(i);
+						JSONObject child = allCategories.get(childID);
+						tree.append("       " + child.getString("Name") + "\n");
+					}
+					
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
+		layoutParams.removeRule(RelativeLayout.CENTER_IN_PARENT);
+		textView.setLayoutParams(layoutParams);
+		textView.setText(tree.toString());
 	}
 
 }
