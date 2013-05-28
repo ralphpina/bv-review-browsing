254cddb9b9034074214bbbe8c972783cafb6357e
ralph.pina <ralph.pina@gmail.com>
2013-05-28 13:26:37 -0500

    adding a textView for each subcategory, putting a listener

--- src/com/bazaarvoice/example/bvreviewbrowsing/MainActivity.java
+++ src/com/bazaarvoice/example/bvreviewbrowsing/MainActivity.java
@@ -9,12 +9,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bazaarvoice.BazaarRequest;
 import com.bazaarvoice.DisplayParams;
@@ -24,7 +29,7 @@ import com.bazaarvoice.types.RequestType;
 
 public class MainActivity extends Activity {
 
-	//private static final String TAG = "MainActivity";
+	private static final String TAG = "MainActivity";
 	
 	private TextView textView;
 	private Map<String, JSONObject> allCategories;
@@ -33,6 +38,7 @@ public class MainActivity extends Activity {
 	private int initialItemCount;
 	private int processedItemCount;
 	private int batchSize;
+	private String selectionID;
 	
 	private JSONArray itemsToProcess;
 	
@@ -40,15 +46,24 @@ public class MainActivity extends Activity {
 	private DisplayParams params;
 	
 	RelativeLayout.LayoutParams layoutParams;
+	RelativeLayout relativeLayout;
+	LinearLayout linearLayout;
+	
+	private Activity context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		context = this;
 	
+		relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+		linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
 		textView = (TextView) findViewById(R.id.textView);
+		
 		textView.setText("Yo! We are loading your data!");
-		textView.setMovementMethod(new ScrollingMovementMethod());
+		//textView.setMovementMethod(new ScrollingMovementMethod());
 
 		layoutParams = (RelativeLayout.LayoutParams)textView.getLayoutParams();
 		layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, 1);
@@ -59,6 +74,9 @@ public class MainActivity extends Activity {
 		processedItemCount = 0;
 		batchSize = 100;
 
+		selectionID = null;
+		
+		Log.e(TAG, "before going into execParseTopCategories");
 		execParseTopCategories();
 		
 	}
@@ -83,18 +101,20 @@ public class MainActivity extends Activity {
 		params.setOffset(processedItemCount);
 		params.setLimit(batchSize);
 		params.addSort("Name", true);
-		params.addFilter("ParentId", Equality.EQUAL, "null");
+		if (selectionID == null) {
+			params.addFilter("ParentId", Equality.EQUAL, "null");
+		} else {
+			params.addFilter("ParentId", Equality.EQUAL, selectionID);
+		}
 		
 		/*
 		 * This request will get first 100
 		 */
+		Log.e(TAG, "before sendDisplayRequest");
 		request.sendDisplayRequest(RequestType.CATEGORIES, params, response);
 	}
 	
 	private void execParseTopCategories() {
-		
-		initialItemCount = 0;
-		processedItemCount = 0;
 		
 		/*
 		 * We will call the method and pass it the listener to execute when the
@@ -105,6 +125,9 @@ public class MainActivity extends Activity {
 			@Override
 			public void onUiResponse(JSONObject response) {
 				try {
+					if (initialItemCount == 0) {
+						initialItemCount = response.getInt("TotalResults");
+					}
 					itemsToProcess = response.getJSONArray("Results");
 				} catch (JSONException e1) {
 					// TODO Auto-generated catch block
@@ -127,9 +150,13 @@ public class MainActivity extends Activity {
 					processedItemCount++;
 				}
 				
+				Log.e(TAG, "processedItemCount = " + processedItemCount);
+				
 				if (initialItemCount > processedItemCount) {
 					execParseTopCategories();
 				} else {
+					initialItemCount = 0;
+					processedItemCount = 0;
 					execParseChildrenCategories();
 				}
 			}
@@ -138,6 +165,8 @@ public class MainActivity extends Activity {
 	}
 	
 	private void parseChildrenCategories(OnBazaarResponse response) {
+		
+		Log.e(TAG, "entered parseChildrenCategories");
 		 
 		/*
 		 * BazaarRequest will be used to make our API calls. The parameters are set in the BVReviewBrowsingApplication class
@@ -175,14 +204,14 @@ public class MainActivity extends Activity {
 	
 	private void execParseChildrenCategories() {
 		
-		initialItemCount = 0;
-		processedItemCount = 0;
-		
 		parseChildrenCategories(new BazaarUIThreadResponse(this) {
 			
 			@Override
 			public void onUiResponse(JSONObject response) {
 				try {
+					if (initialItemCount == 0) {
+						initialItemCount = response.getInt("TotalResults");
+					}
 					itemsToProcess = response.getJSONArray("Results");
 				} catch (JSONException e1) {
 					// TODO Auto-generated catch block
@@ -212,6 +241,8 @@ public class MainActivity extends Activity {
 					processedItemCount++;
 				}
 				
+				Log.e(TAG, "processedItemCount in  execParseChildrenCategories = " + processedItemCount);
+				
 				if (initialItemCount > processedItemCount) {
 					execParseChildrenCategories();
 				} else {
@@ -222,25 +253,57 @@ public class MainActivity extends Activity {
 		
 	}
 	
-	//TODO remove this
+	
 	@SuppressLint("NewApi")
 	private void displayCategories() {
-		StringBuilder tree = new StringBuilder();
 		boolean hasChildren;
 		JSONArray children;
+		TextView newTextView;
+		
+		Log.e(TAG, "got to displayCategories()");
+		
+		//hide other textView 
+		this.relativeLayout.removeView(textView);
+		
+		//layout parameters for my new TextViews
+		LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		
 		
 		for (JSONObject obj : allCategories.values()) {
 			try {
 				hasChildren = obj.getBoolean("HasChildren");
 				
 				if (hasChildren) {
-					tree.append(obj.getString("Name") + "\n");
+					newTextView = new TextView(this);
+					newTextView.setLayoutParams(layoutParams);
+					newTextView.setText(obj.getString("Name"));
+					this.linearLayout.addView(newTextView);
+					
 					children = obj.getJSONArray("Children");
 					
 					for (int i = 0; i < children.length(); i++) {
 						String childID = (String) children.get(i);
-						JSONObject child = allCategories.get(childID);
-						tree.append("       " + child.getString("Name") + "\n");
+						final JSONObject child = allCategories.get(childID);
+						
+						newTextView = new TextView(this);
+						newTextView.setLayoutParams(layoutParams);
+						newTextView.setText("       " + child.getString("Name"));
+						newTextView.setOnClickListener(new OnClickListener() {
+							String idClicked = child.getString("Id");
+							@Override
+							public void onClick(View v) {
+								selectionID = idClicked;
+								//change the dispplay
+								//linearLayout.removeAllViews();
+								//relativeLayout.addView(textView);
+								
+								initialItemCount = 0;
+								processedItemCount = 0;								
+								//execParseTopCategories();
+								Toast.makeText(context, "name = " + ((TextView) v).getText() + " and id = " + idClicked, Toast.LENGTH_LONG).show();
+							}
+						});
+						this.linearLayout.addView(newTextView);
 					}
 					
 				}
@@ -249,10 +312,11 @@ public class MainActivity extends Activity {
 				e.printStackTrace();
 			}
 		}
+		//relativeLayout.refreshDrawableState();
+	}
+	
+	public void loadSelection(View v) {
 		
-		layoutParams.removeRule(RelativeLayout.CENTER_IN_PARENT);
-		textView.setLayoutParams(layoutParams);
-		textView.setText(tree.toString());
 	}
 
 }
