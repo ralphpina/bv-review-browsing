5acd697132c1b146ee95cf5e49629041995cf8bf
ralph.pina <ralph.pina@gmail.com>
2013-05-28 22:29:30 -0500

    pulling in all categories, now working on logic for products

--- src/com/bazaarvoice/example/bvreviewbrowsing/MainActivity.java
+++ src/com/bazaarvoice/example/bvreviewbrowsing/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bazaarvoice.example.bvreviewbrowsing;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -33,14 +34,15 @@ public class MainActivity extends Activity {
 	
 	private TextView textView;
 	private Map<String, JSONObject> allCategories;
+	private Map<String, JSONObject> allProducts;
+	private ArrayList<String> topCategoryIds;
+	private JSONArray itemsToProcess;
 	
 	
 	private int initialItemCount;
 	private int processedItemCount;
 	private int batchSize;
 	private String selectionID;
-	
-	private JSONArray itemsToProcess;
 	
 	private BazaarRequest request;
 	private DisplayParams params;
@@ -89,6 +91,9 @@ public class MainActivity extends Activity {
 	}
 	
 	private void parseTopCategories(OnBazaarResponse response) {
+		
+		Log.e(TAG, "entered parseTopCategories");
+		
 		/*
 		 * BazaarRequest will be used to make our API calls. The parameters are set in the BVReviewBrowsingApplication class
 		 */
@@ -101,6 +106,8 @@ public class MainActivity extends Activity {
 		params.setOffset(processedItemCount);
 		params.setLimit(batchSize);
 		params.addSort("Name", true);
+		Log.e(TAG, "parseTopCategories : selectionID = " + selectionID);
+		
 		if (selectionID == null) {
 			params.addFilter("ParentId", Equality.EQUAL, "null");
 		} else {
@@ -110,11 +117,12 @@ public class MainActivity extends Activity {
 		/*
 		 * This request will get first 100
 		 */
-		Log.e(TAG, "before sendDisplayRequest");
 		request.sendDisplayRequest(RequestType.CATEGORIES, params, response);
 	}
 	
 	private void execParseTopCategories() {
+		
+		Log.e(TAG, "entered execParseTopCategories");
 		
 		/*
 		 * We will call the method and pass it the listener to execute when the
@@ -124,6 +132,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onUiResponse(JSONObject response) {
+				Log.e(TAG, "execParseTopCategories : onUiResponse");
 				try {
 					if (initialItemCount == 0) {
 						initialItemCount = response.getInt("TotalResults");
@@ -136,13 +145,18 @@ public class MainActivity extends Activity {
 				
 				JSONObject currentObj;
 				JSONArray children;
+				topCategoryIds = new ArrayList<String>();
+				
 				for (int i = 0; i < itemsToProcess.length(); i++) {
 					try {
 						currentObj = itemsToProcess.getJSONObject(i);
 						children = new JSONArray();
 						currentObj.put("Children", children);
 						currentObj.put("HasChildren", false);
+						//put the item into our flat map with all categories
 						allCategories.put(currentObj.getString("Id"), currentObj);
+						//put the items into our arraylist to then query their children
+						topCategoryIds.add(currentObj.getString("Id"));
 					} catch (JSONException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -150,7 +164,7 @@ public class MainActivity extends Activity {
 					processedItemCount++;
 				}
 				
-				Log.e(TAG, "processedItemCount = " + processedItemCount);
+				Log.e(TAG, "execParseTopCategories : processedItemCount = " + processedItemCount);
 				
 				if (initialItemCount > processedItemCount) {
 					execParseTopCategories();
@@ -179,19 +193,14 @@ public class MainActivity extends Activity {
 		params = new DisplayParams();
 		params.setOffset(processedItemCount);
 		params.setLimit(batchSize);
-		ArrayList<String> topCategoryIds = new ArrayList<String>();
 		
-		for (JSONObject id : allCategories.values()) {
-			try {
-				topCategoryIds.add(id.getString("Id"));
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		Log.e(TAG, "parseChildrenCategories : selectionID = " + selectionID);
 		
 		String[] ids = new String[topCategoryIds.size()];
 		topCategoryIds.toArray(ids);
+		
+		//TODO remove
+		Log.e(TAG, "parseChildrenCategories: ids = " + Arrays.toString(ids));
 		
 		params.addFilter("ParentId", Equality.EQUAL, ids);
 		
@@ -213,54 +222,109 @@ public class MainActivity extends Activity {
 						initialItemCount = response.getInt("TotalResults");
 					}
 					itemsToProcess = response.getJSONArray("Results");
+					
+					//if there are no results means that these items have to child categories
+					//they just have products
+					if (itemsToProcess == null) {
+						execParseProducts();
+						
+					//go get the child categories
+					} else { 
+						
+						//TODO remove
+						Log.e(TAG, "execParseChildrenCategories: itemsToProcess = " + itemsToProcess);
+						Log.e(TAG, "processedItemCount in  execParseChildrenCategories = " + processedItemCount);					
+						
+						JSONObject currentObj;
+						JSONArray children;
+						JSONObject parent;
+						for (int i = 0; i < itemsToProcess.length(); i++) {
+							
+							currentObj = itemsToProcess.getJSONObject(i);
+							children = new JSONArray();
+							currentObj.put("Children", children);
+							currentObj.put("HasChildren", false);
+							allCategories.put(currentObj.getString("Id"), currentObj);
+							
+							parent = allCategories.get(currentObj.getString("ParentId"));
+							parent.put("HasChildren", true);
+							children = parent.getJSONArray("Children");
+							children.put(currentObj.getString("Id"));
+											
+							processedItemCount++;
+						}
+						
+						Log.e(TAG, "processedItemCount in  execParseChildrenCategories = " + processedItemCount);
+						
+						if (initialItemCount > processedItemCount) {
+							execParseChildrenCategories();
+						} else {
+							initialItemCount = 0;
+							processedItemCount = 0;
+							displayCategories();
+						}
+						
+					}
 				} catch (JSONException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
-				}
-				
-				JSONObject currentObj;
-				JSONArray children;
-				JSONObject parent;
-				for (int i = 0; i < itemsToProcess.length(); i++) {
-					try {
-						currentObj = itemsToProcess.getJSONObject(i);
-						children = new JSONArray();
-						currentObj.put("Children", children);
-						currentObj.put("HasChildren", false);
-						allCategories.put(currentObj.getString("Id"), currentObj);
-						
-						parent = allCategories.get(currentObj.getString("ParentId"));
-						parent.put("HasChildren", true);
-						children = parent.getJSONArray("Children");
-						children.put(currentObj.getString("Id"));
-						
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}			
-					processedItemCount++;
-				}
-				
-				Log.e(TAG, "processedItemCount in  execParseChildrenCategories = " + processedItemCount);
-				
-				if (initialItemCount > processedItemCount) {
-					execParseChildrenCategories();
-				} else {
-					displayCategories();
 				}
 			}
 		});	
+	}
+	
+	private void parseProducts(OnBazaarResponse response) {
 		
+		Log.e(TAG, "entered parseProducts");
+		 
+		/*
+		 * BazaarRequest will be used to make our API calls. The parameters are set in the BVReviewBrowsingApplication class
+		 */
+		request = new BazaarRequest(BVReviewBrowsingApplication.domain, BVReviewBrowsingApplication.passKey, BVReviewBrowsingApplication.apiVersion);
+		
+		/*
+		 * Get all the top child categories
+		 */
+		params = new DisplayParams();
+		params.setOffset(processedItemCount);
+		params.setLimit(batchSize);
+		
+		Log.e(TAG, "parseChildrenCategories : selectionID = " + selectionID);
+		
+		String[] ids = new String[topCategoryIds.size()];
+		topCategoryIds.toArray(ids);
+		
+		//TODO remove
+		Log.e(TAG, "parseChildrenCategories: ids = " + Arrays.toString(ids));
+		
+		params.addFilter("ParentId", Equality.EQUAL, ids);
+		
+		/*
+		 * This request will get first 100
+		 */
+		request.sendDisplayRequest(RequestType.CATEGORIES, params, response);
+		
+	}
+	
+	private void execParseProducts() {
+		
+		parseProducts(new BazaarUIThreadResponse(this) {
+			
+			@Override
+			public void onUiResponse(JSONObject response) {
+				
+			}
+		});
 	}
 	
 	
 	@SuppressLint("NewApi")
 	private void displayCategories() {
-		boolean hasChildren;
+		//boolean hasChildren;
 		JSONArray children;
 		TextView newTextView;
+		JSONObject obj;
 		
-		Log.e(TAG, "got to displayCategories()");
+		Log.e(TAG, "entered displayCategories()");
 		
 		//hide other textView 
 		this.relativeLayout.removeView(textView);
@@ -268,50 +332,104 @@ public class MainActivity extends Activity {
 		//layout parameters for my new TextViews
 		LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		
+		/*
+		String parent;
+		if (selectionID == null) {
+			parent = "null";
+		} else {
+			parent = selectionID;
+		}
+		*/
 		
-		for (JSONObject obj : allCategories.values()) {
+		for (String topId : topCategoryIds) {
 			try {
-				hasChildren = obj.getBoolean("HasChildren");
+				obj = allCategories.get(topId);
+				newTextView = new TextView(this);
+				newTextView.setLayoutParams(layoutParams);
+				newTextView.setText(obj.getString("Name"));
+				this.linearLayout.addView(newTextView);
 				
-				if (hasChildren) {
+				children = obj.getJSONArray("Children");
+				
+				for (int i = 0; i < children.length(); i++) {
+					String childID = (String) children.get(i);
+					final JSONObject child = allCategories.get(childID);
+					
 					newTextView = new TextView(this);
 					newTextView.setLayoutParams(layoutParams);
-					newTextView.setText(obj.getString("Name"));
+					newTextView.setText("       " + child.getString("Name"));
+					newTextView.setOnClickListener(new OnClickListener() {
+						String idClicked = child.getString("Id");
+						@Override
+						public void onClick(View v) {
+							selectionID = idClicked;
+							//change the dispplay
+							linearLayout.removeAllViews();
+							relativeLayout.addView(textView);
+							
+							initialItemCount = 0;
+							processedItemCount = 0;								
+							execParseTopCategories();
+							Toast.makeText(context, "name = " + ((TextView) v).getText() + " and id = " + idClicked, Toast.LENGTH_LONG).show();
+						}
+					});
 					this.linearLayout.addView(newTextView);
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		/*
+		for (JSONObject obj : allCategories.values()) {
+			try {
+				
+				if (parent.equals(obj.getString("ParentId"))) {
+				
+					hasChildren = obj.getBoolean("HasChildren");
 					
-					children = obj.getJSONArray("Children");
-					
-					for (int i = 0; i < children.length(); i++) {
-						String childID = (String) children.get(i);
-						final JSONObject child = allCategories.get(childID);
-						
+					if (hasChildren) {
 						newTextView = new TextView(this);
 						newTextView.setLayoutParams(layoutParams);
-						newTextView.setText("       " + child.getString("Name"));
-						newTextView.setOnClickListener(new OnClickListener() {
-							String idClicked = child.getString("Id");
-							@Override
-							public void onClick(View v) {
-								selectionID = idClicked;
-								//change the dispplay
-								//linearLayout.removeAllViews();
-								//relativeLayout.addView(textView);
-								
-								initialItemCount = 0;
-								processedItemCount = 0;								
-								//execParseTopCategories();
-								Toast.makeText(context, "name = " + ((TextView) v).getText() + " and id = " + idClicked, Toast.LENGTH_LONG).show();
-							}
-						});
+						newTextView.setText(obj.getString("Name"));
 						this.linearLayout.addView(newTextView);
+						
+						children = obj.getJSONArray("Children");
+						
+						for (int i = 0; i < children.length(); i++) {
+							String childID = (String) children.get(i);
+							final JSONObject child = allCategories.get(childID);
+							
+							newTextView = new TextView(this);
+							newTextView.setLayoutParams(layoutParams);
+							newTextView.setText("       " + child.getString("Name"));
+							newTextView.setOnClickListener(new OnClickListener() {
+								String idClicked = child.getString("Id");
+								@Override
+								public void onClick(View v) {
+									selectionID = idClicked;
+									//change the dispplay
+									linearLayout.removeAllViews();
+									relativeLayout.addView(textView);
+									
+									initialItemCount = 0;
+									processedItemCount = 0;								
+									execParseTopCategories();
+									Toast.makeText(context, "name = " + ((TextView) v).getText() + " and id = " + idClicked, Toast.LENGTH_LONG).show();
+								}
+							});
+							this.linearLayout.addView(newTextView);
+						}
+						
 					}
-					
 				}
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+		*/
 		//relativeLayout.refreshDrawableState();
 	}
 	
